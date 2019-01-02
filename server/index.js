3f808af1e5322041b7af233d3b70afba1eb072d4
denis.khaydarshin <dragolietel@gmail.com>
2019-01-03 00:36:38 +0300

    improved server js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 4000;
 
-app.use(express.static(__dirname + '/../build'));
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+});
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
