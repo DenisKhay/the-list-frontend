8e5d02e6765076031cc8cb700c290c12c0390de1
denis.khaydarshin <dragolietel@gmail.com>
2019-01-04 23:26:32 +0300

    added react-router-dom. fixed vuln

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
-
-const client = new ApolloClient({
-  uri: "http://localhost:8087/api"
-});
-
-client
-  .query({
-    query: gql`
-      {
-        books {
-          name
-          author {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
 class App extends Component {
   constructor(props: any) {
     super(props);
