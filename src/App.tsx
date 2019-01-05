20303c59f72e162068ad402c06e44013addc9ec6
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2019-01-05 12:32:32 +0300

    added single quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   constructor(props: any) {
